d13ffd457297cbc1e4bba3b8326ffb91e47b038d
Josh Zirena <JoshZirena@gmail.com>
2020-05-11 16:40:33 +0200

    feat: create post-app

---
+++ StackOverflow_Post_App/Post.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Post_App
+{
+    public class Post
+    {
+        private DateTime _creationDate;
+        private int _totalVotes = 0;
+        private string _title;
+        private string _description;
+
+        public Post(string title, string description)
+        {
+            this._title = title;
+            this._description = description;
+        }
+
+        public void CreatePost()
+        {
+            this._creationDate = DateTime.Now;
+        }
+
+        public void upVote()
+        {
+            this._totalVotes++;
+        }
+
+        public void downVote()
+        {
+            this._totalVotes--; 
+        }
+
+        public void readSummary()
+        {
+            Console.WriteLine("Title: " + _title);
+            Console.WriteLine("Description: " + _description);
+            Console.WriteLine("Date created: "  + _creationDate);
+            Console.WriteLine("totalVotes: " + _totalVotes);
+        }
+    }
+}
---
+++ StackOverflow_Post_App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Post_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a post
+            var postTitle = "How I got a job as a Software Engineer.";
+            var description = "Look, anyone can do it. All you need is a little bit of...";
+
+            var firstPost = new Post(postTitle, description);
+            firstPost.CreatePost();
+            firstPost.upVote();
+            firstPost.upVote();
+            firstPost.upVote();
+            firstPost.downVote();
+            firstPost.readSummary();
+        }
+    }
+}
